c20d80c29730cf32167dcb2fc3f0e762147f66e6
LeMinHo <47167976+LeMinHo@users.noreply.github.com>
2020-02-29 11:54:37 +0630

    Add files via upload

---
+++ TestingSession.php
@@ -0,0 +1,6 @@
+<a href="home.php" >Home</a>|<br>
+<a href="profile.php">Profile(Checking username and pass ,anf then enter the page)</a> |<br>
+<a href="contact.php">Contact(Checking username and pass ,anf then enter the page)</a> |<br>
+<a href="about.php">About(Destory session)</a> 
+
+<br><br>
